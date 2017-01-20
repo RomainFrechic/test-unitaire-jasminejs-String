2d134b4fa770f2f13640a7561b6b7a4c40d84a87
Romain <RomainFrechic@outlook.fr>
2017-01-20 15:14:22 +0100

    test unitaire fizzbuzz ok

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -1,9 +1,21 @@
-var FizzBuzzOf = require('../lib/FizzBuzz').FizzBuzzOf
+var FizzBuzzOf = require('../lib/fizzBuzz').FizzBuzzOf
 
 describe("FizzBuzzOf execution", function () {
 
-    it("should return ChangeMe at the beginning with parameter 1", function () {
-        expect(FizzBuzzOf(1)).toBe("ChangeMe");
+    it("should return 1 at the beginning with parameter 1", function () {
+        expect(FizzBuzzOf(1)).toBe(1);
+    });
+    it("should return 2 with parameter 2", function () {
+        expect(FizzBuzzOf(2)).toBe(2);
+    });
+    it("should return Fizz with parameter 3", function () {
+        expect(FizzBuzzOf(3)).toBe("Fizz");
+    });
+    it("should return Buzz with parameter 5", function () {
+        expect(FizzBuzzOf(5)).toBe("Buzz");
+    });
+    it("should return FizzBuzz with parameter 3 and 5", function () {
+        expect(FizzBuzzOf(3)+FizzBuzzOf(5)).toBe("FizzBuzz");
     });
 
 });
